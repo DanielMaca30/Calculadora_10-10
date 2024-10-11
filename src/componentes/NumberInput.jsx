f8e9a15b13c3c27129fec1e6bfce2dd40e792108
DanielMaca30 <daniel.maca@uao.edu.co>
2024-10-10 20:57:25 -0500

    calculadora con 4 calculos

---
+++ src/componentes/NumberInput.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Resultado from './Resultado';
+import { operaciones } from '../helpers/operaciones';
+
+const NumberInput = () => {
+  const [numeros, setNumeros] = useState({
+    numero1: 0,
+    numero2: 0
+  });
+  
+  const { handleChange, suma, resta, multiplicacion, division, numero1, numero2 } = operaciones(numeros, setNumeros);
+
+  return (
+    <>
+      <label>
+        Numero 1:
+        <input name='numero1' value={numero1} onChange={handleChange} type="number" />
+      </label>
+      <label>
+        Numero 2:
+        <input name='numero2' value={numero2} onChange={handleChange} type="number" />
+      </label>
+      <Resultado operacion="Suma" calculo={suma()} />
+      <Resultado operacion="Resta" calculo={resta()} />
+      <Resultado operacion="Multiplicación" calculo={multiplicacion()} />
+      <Resultado operacion="División" calculo={division()} />
+    </>
+  );
+};
+
+NumberInput.propTypes = {
+  name: PropTypes.string,
+};
+
+export default NumberInput;
---
+++ src/helpers/operaciones.js
@@ -0,0 +1,18 @@
+export const operaciones = (numeros, setNumeros) => {
+    const { numero1, numero2 } = numeros;
+  
+    const handleChange = (e) => {
+      setNumeros({
+        ...numeros,
+        [e.target.name]: parseFloat(e.target.value),
+      });
+    };
+  
+    const suma = () => numero1 + numero2;
+    const resta = () => numero1 - numero2;
+    const multiplicacion = () => numero1 * numero2;
+    const division = () => (numero2 !== 0 ? numero1 / numero2 : 'Error'); // Evitar división por cero
+  
+    return { handleChange, suma, resta, multiplicacion, division, numero1, numero2 };
+  };
+  
